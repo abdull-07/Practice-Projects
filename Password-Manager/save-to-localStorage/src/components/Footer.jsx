8aaaa5a6c9aaeb8806c585e0591c1bc42ad19372
abdull-07 <personal.muhammadabdullah@gmail.com>
2025-02-23 09:38:35 +0500

    Make Responsive

--- Password-Manager/save-to-localStorage/src/components/Footer.jsx
+++ Password-Manager/save-to-localStorage/src/components/Footer.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='bg-cyan-100 absolute bottom w-full overflow-x-hidden'>
-            <div className="mycontainer flex text-black justify-between items-center font-bold italic py-2 max-w-4xl">
+        <footer className='bg-cyan-100 absolute bottom-0 w-full overflow-x-hidden'>
+            <div className="mycontainer flex flex-col md:flex-row text-black justify-between items-center font-bold italic py-2 max-w-4xl mx-auto">
                 <div>
                     <h1>Key<span className='text-cyan-700'>Vault</span></h1>
                 </div>
-                <div>
-                    <h1> Made with ❤ by <span className='text-cyan-700 font-bold italic'>Abdul</span>.<span className='text-yellow-700 font-bold italic'> Stay secure</span>,<span className='text-green-700 font-bold italic'> stay safe!</span></h1>
+                <div className='mt-2 md:mt-0 text-center md:text-left'>
+                    <h1>
+                        Made with ❤ by <span className='text-cyan-700 font-bold italic'>Abdul</span>.
+                        <span className='text-yellow-700 font-bold italic'> Stay secure</span>,
+                        <span className='text-green-700 font-bold italic'> stay safe!</span>
+                    </h1>
                 </div>
             </div>
         </footer>
--- Password-Manager/save-to-localStorage/src/components/Manager.jsx
+++ Password-Manager/save-to-localStorage/src/components/Manager.jsx
@@ -21,7 +21,6 @@ const Manager = () => {
     }, [])
 
     const showPassword = () => {
-        refPassword.current.type = "password"
         if (ref.current.src.includes("icons/show-password.svg")) {
             ref.current.src = "icons/hide-password.svg"
             refPassword.current.type = "password"
@@ -137,9 +136,9 @@ const Manager = () => {
 
             <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
 
-            <div className="max-w-5xl mycontainer ">
+            <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
                 <h1 className='text-2xl font-bold italic text-center'>Key<span className='text-cyan-700'>Vault</span></h1>
-                <p className='text-cyan-700 font-bold text-sx'>Your own secure vault for all your keys and passwords</p>
+                <p className='text-cyan-700 font-bold text-sx text-center'>Your own secure vault for all your keys and passwords</p>
                 <button onClick={showMyManager} className='my-5 show-mymanager flex items-center justify-center gap-2 bg-cyan-700 text-white rounded-full p-3 py-1 w-fit border-2 hover:border-cyan-900 hover:bg-cyan-600 transition-all duration-300 ease-in-out'>
                     <lord-icon
                         src="https://cdn.lordicon.com/sbnjyzil.json"
@@ -149,9 +148,9 @@ const Manager = () => {
                 </button>
                 <div className="flex flex-col gap-8 py-5 items-center mymanager hidden">
                     <input onChange={handelChange} value={form.site} className='rounded-full border border-cyan-900 p-3 py-1 w-full' type="text" name="site" placeholder='example.com' required />
-                    <div className="flex gap-4 w-full justify-between">
+                    <div className="flex flex-col sm:flex-row gap-4 w-full justify-between">
                         <input onChange={handelChange} value={form.username} className='rounded-full w-full border border-cyan-900 p-3 py-1' type="text" name="username" placeholder='Email / username' required />
-                        <div className="relative w-1/2">
+                        <div className="relative w-full sm:w-1/2">
                             <input onChange={handelChange} value={form.password} ref={refPassword} className='rounded-full w-full border border-cyan-900 p-3 py-1' type="password" name="password" placeholder='password' required />
                             <span className="absolute right-[5px] top-[6px] cursor-pointer" onClick={showPassword}>
                                 <img ref={ref} src="icons/hide-password.svg" alt="show-password" />
@@ -169,80 +168,84 @@ const Manager = () => {
                     </button>
                 </div>
                 <hr />
-                <div>
-                    <div className='flex py-5'><h2 className='font-bold text-[larger] italic w-1/3'>Your Saved <span className='text-cyan-700 underline'>Passwords</span></h2>
+                <div className='pb-8'>
+                    <div className='flex flex-col sm:flex-row py-5'>
+                        <h2 className='font-bold text-[larger] italic w-full sm:w-1/3'>Your Saved <span className='text-cyan-700 underline'>Passwords</span></h2>
                         {filteredPasswords.length !== 0 && <input
                             type="text" placeholder="Search..." value={searchQuery} onChange={handleSearchChange} className="rounded-full border border-cyan-900 p-3 py-1 w-full"
-                        />}</div>
+                        />}
+                    </div>
                     {filteredPasswords.length === 0 && <p className='text-center text-red-700 font-bold italic text-2xl underline'>No Passwords Found</p>}
                     {filteredPasswords.length !== 0 && (
-                        <table className="table-auto w-full text-center border-l-[1px] border-r-[1px] border-cyan-700">
-                            <thead className="bg-cyan-800 text-white">
-                                <tr>
-                                    <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700">Site</th>
-                                    <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700">Email / UserName</th>
-                                    <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700">Password</th>
-                                    <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700">Notes</th>
-                                    <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700 border-collapse">Actions</th>
-                                </tr>
-                            </thead>
-
-                            <tbody className="bg-cyan-100">
-                                {filteredPasswords.map((item, index) => (
-                                    <tr key={index}>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-cyan-700 hover:bg-cyan-300">
-                                            <a href={item.site} target="_blank" rel="noopener noreferrer" className="text-cyan-700 underline">
-                                                {item.site}
-                                            </a>
-                                        </td>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-cyan-700 hover:bg-cyan-300">
-                                            <div className="inline-flex items-center gap-2" onClick={() => copyText(item.username)}>
-                                                {item.username}
-                                                <lord-icon
-                                                    src="https://cdn.lordicon.com/iykgtsbt.json"
-                                                    trigger="hover"
-                                                    colors="primary:#0891b2,secondary:#cffafe"
-                                                    className="w-5 h-5 cursor-pointer"
-                                                ></lord-icon>
-                                            </div>
-                                        </td>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-cyan-700 hover:bg-cyan-300">
-                                            <div className="inline-flex items-center gap-2" onClick={() => copyText(item.password)}>
-                                                {item.password}
-                                                <lord-icon
-                                                    src="https://cdn.lordicon.com/iykgtsbt.json"
-                                                    trigger="hover"
-                                                    colors="primary:#0891b2,secondary:#cffafe"
-                                                    className="w-5 h-5 cursor-pointer"
-                                                ></lord-icon>
-                                            </div>
-                                        </td>
-                                        <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-cyan-700 hover:bg-cyan-300">
-                                            {item.notes}
-                                        </td>
-                                        <td className='border-b-[1px]  border-cyan-700'>
-                                            <span  className='mr-4 cursor-pointer' onClick={() => updateRecord(item.id)}>
-                                                <lord-icon
-                                                    src="https://cdn.lordicon.com/exymduqj.json"
-                                                    trigger="hover"
-                                                    colors="primary:#0e7490,secondary:#164e63"
-                                                    className="cursor-pointer w-6 h-6">
-                                                </lord-icon>
-                                            </span>
-                                            <span onClick={() => deleteRecord(item.id)} className='cursor-pointer'>
-                                                <lord-icon
-                                                    src="https://cdn.lordicon.com/hwjcdycb.json"
-                                                    trigger="hover"
-                                                    colors="primary:#0e7490,secondary:#164e63"
-                                                    className="cursor-pointer w-6 h-6">
-                                                </lord-icon>
-                                            </span>
-                                        </td>
-
+                        <div className="overflow-x-auto">
+                            <table className="table-auto w-full text-center border-l-[1px] border-r-[1px] border-cyan-700">
+                                <thead className="bg-cyan-800 text-white">
+                                    <tr>
+                                        <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700">Site</th>
+                                        <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700">Email / UserName</th>
+                                        <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700">Password</th>
+                                        <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700">Notes</th>
+                                        <th scope="col" className="py-2 px-4 border-b-[1px] border-cyan-700 border-collapse">Actions</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+
+                                <tbody className="bg-cyan-100">
+                                    {filteredPasswords.map((item, index) => (
+                                        <tr key={index}>
+                                            <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-cyan-700 hover:bg-cyan-300">
+                                                <a href={item.site} target="_blank" rel="noopener noreferrer" className="text-cyan-700 underline">
+                                                    {item.site}
+                                                </a>
+                                            </td>
+                                            <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-cyan-700 hover:bg-cyan-300">
+                                                <div className="inline-flex items-center gap-2" onClick={() => copyText(item.username)}>
+                                                    {item.username}
+                                                    <lord-icon
+                                                        src="https://cdn.lordicon.com/iykgtsbt.json"
+                                                        trigger="hover"
+                                                        colors="primary:#0891b2,secondary:#cffafe"
+                                                        className="w-5 h-5 cursor-pointer"
+                                                    ></lord-icon>
+                                                </div>
+                                            </td>
+                                            <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-cyan-700 hover:bg-cyan-300">
+                                                <div className="inline-flex items-center gap-2" onClick={() => copyText(item.password)}>
+                                                    {item.password}
+                                                    <lord-icon
+                                                        src="https://cdn.lordicon.com/iykgtsbt.json"
+                                                        trigger="hover"
+                                                        colors="primary:#0891b2,secondary:#cffafe"
+                                                        className="w-5 h-5 cursor-pointer"
+                                                    ></lord-icon>
+                                                </div>
+                                            </td>
+                                            <td className="py-2 px-4 border-b-[1px] border-r-[1px] border-cyan-700 hover:bg-cyan-300">
+                                                {item.notes}
+                                            </td>
+                                            <td className='border-b-[1px]  border-cyan-700'>
+                                                <span  className='mr-4 cursor-pointer' onClick={() => updateRecord(item.id)}>
+                                                    <lord-icon
+                                                        src="https://cdn.lordicon.com/exymduqj.json"
+                                                        trigger="hover"
+                                                        colors="primary:#0e7490,secondary:#164e63"
+                                                        className="cursor-pointer w-6 h-6">
+                                                    </lord-icon>
+                                                </span>
+                                                <span onClick={() => deleteRecord(item.id)} className='cursor-pointer'>
+                                                    <lord-icon
+                                                        src="https://cdn.lordicon.com/hwjcdycb.json"
+                                                        trigger="hover"
+                                                        colors="primary:#0e7490,secondary:#164e63"
+                                                        className="cursor-pointer w-6 h-6">
+                                                    </lord-icon>
+                                                </span>
+                                            </td>
+
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     )}
                 </div>
             </div>
